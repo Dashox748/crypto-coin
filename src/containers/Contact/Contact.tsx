ecdde9c907760222a9e785abb8573744f20e84c0
Hubert Grabke <hgrabke@wp.pl>
2022-12-19 19:01:11 +0100

    added section Contact

--- src/containers/Contact/Contact.tsx
+++ src/containers/Contact/Contact.tsx
@@ -1,8 +1,19 @@
+import {Button, Container} from "@mui/material"
+import TextField from "@mui/material/TextField";
 
-const Contact =()=>{
-    return(
-            <div>Contact</div>
-            )
+
+const Contact = () => {
+    return (
+        <Container maxWidth="xl" sx={{display: "flex", flexDirection: "column", gap: "2rem"}} component="form"
+                   noValidate
+                   autoComplete="off"
+        >
+            <TextField placeholder="Your Addres Email" fullWidth/>
+            <TextField placeholder="Your Message" rows={4} fullWidth multiline/>
+            <Button variant="contained" size="large">Send</Button>
+
+        </Container>
+    )
 }
 
 export default Contact
