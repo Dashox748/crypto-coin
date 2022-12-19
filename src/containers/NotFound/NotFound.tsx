9f5667997a9a3c7ded0fe6f9300350bc6389a9c7
Hubert Grabke <hgrabke@wp.pl>
2022-12-19 19:01:50 +0100

    added NotFound website when someone is not on correct link

--- src/containers/NotFound/NotFound.tsx
+++ src/containers/NotFound/NotFound.tsx
@@ -1,8 +1,20 @@
+import {Box,Typography,Button } from "@mui/material"
+
 
 
 const NotFound =()=>{
     return(
-            <div>nima</div>
+            <Box display="flex" justifyContent="center" flexDirection="column" alignItems="center" gap="1rem">
+                <Typography variant="h4" color="#6c757d">Error 404</Typography>
+                <Typography variant="h3" fontWeight="500" color="white">
+                    OOPS WE COULDNT FIND THE PAGE
+                </Typography>
+                <Typography variant="h6" fontWeight="500" color="#6c757d" maxWidth="550px" textAlign="center">
+                    We couldn't fint the page you were looking for, or maybe it never
+                    existed, Try heading back to the home page.
+                </Typography>
+                <Button variant="contained">Back Home</Button>
+            </Box>
     )
 }
 
