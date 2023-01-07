4e165f19bceb3a4040cd762460b67c1a97cf45ee
Hubert Grabke <hgrabke@wp.pl>
2023-01-07 19:23:07 +0100

    packages to hosting control

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import App from "./App";
 import "./index.css";
 import { ProSidebarProvider } from "react-pro-sidebar";
 import { BrowserRouter } from "react-router-dom";
+import { inject } from '@vercel/analytics';
 
+inject();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ProSidebarProvider>
     <BrowserRouter>
